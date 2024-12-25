57b20e6b49e944a97af62a97deef2eebfb0a147a
Elijah Ia <yakamsev@gmail.com>
2024-12-25 17:42:55 +0400

    cron

--- app/initenv.mjs
+++ app/initenv.mjs
@@ -1,6 +1,9 @@
 import path from 'node:path';
 import dotenv from 'dotenv';
+import cron from 'node-cron';
+import shell from 'shelljs';
 import {connectDB, testDB} from "../database/db.mjs";
+import logger from './logger.mjs';
 
 async function ignition(envPath) {
     if (!envPath) {
@@ -13,6 +16,15 @@ async function ignition(envPath) {
     });
     await connectDB();
     await testDB();
+    const youtubeScanScript = process.env.PATHTOYSCAN;
+    logger.debug('path to youtube scan: '+youtubeScanScript);
+    const youtubeScan = cron.schedule('0 0/4 * * *', () =>  {
+        logger.info('Starting Youtube Scan by Cron at ' + Date.now())
+        shell.exec('node ' + youtubeScanScript);
+      }, {
+        scheduled: false
+      });
+    youtubeScan.start();
 }
 
 export default ignition;
