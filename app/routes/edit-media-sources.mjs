9726a500601e421f929d44c0adcc3e0e9522a63a
Elijah Ia <yakamsev@gmail.com>
2025-02-24 01:24:58 +0400

    start media-sources

--- app/routes/edit-media-sources.mjs
+++ app/routes/edit-media-sources.mjs
@@ -14,13 +14,36 @@ const upload = multer();
 const router = express.Router();
 
 router.get('/editor/media-sources/', isLogged, async (request, response) => {
-    const mediaSources = await getMediaSourcesList();
-    const webPageData = {
-      title : "Источники медиа",
-      internalMessage: null,
-      mediaSources : mediaSources
-    };
-    response.render('editor/media-sources.ejs', {webPageData: webPageData});
+    var webPageData = await prepareIndexPageData();
+    webPageData.showJSON = true;
+    response.render('editor/media-sources-edit.ejs', {webPageData: webPageData});
 })
+
+async function prepareIndexPageData() {
+  var pageData = await prepareCleanWebpageData();
+  pageData.title = "Редактирование источников медиа";
+  return pageData;
+}
+
+async function getFullMediaSourcesList() {
+  const result = getfullMediaSourcesListDB();
+  return result.rows;
+}
+
+async function getfullMediaSourcesListDB () {
+  const result = "";
+  return result;
+}
+
+async function prepareCleanWebpageData() {
+  const webPageData = {
+    title: false,
+    internalMessage: false,
+    showJSON: false,
+    pageMenu: false,
+    content: false
+  };
+  return webPageData;
+};
 
 export default router;
